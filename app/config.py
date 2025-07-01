ec6d8e50cf48e681d467499eea12ce06d48011e4
Darlene Santes <darlenesantes@gmail.com>
2025-06-30 19:42:27 -0500

    Added initial GenAI summary feature, updated env variable name

--- app/config.py
+++ app/config.py
@@ -2,13 +2,14 @@
 
 #First we will read our .env file to get our environment variables
 from dotenv import load_dotenv
+import os
+
 load_dotenv()
 
-import os
 
 GOOGLE_BOOKS_KEY = os.getenv('GOOGLE_BOOKS_KEY')
 
-GOOGLE_GENAI_KEY = os.getenv('GOOGLE_GENAI_KEY')
+GEMINI_API_KEY = os.getenv('GOOGLE_GENAI_KEY')
 
 GOOGLE_GENAI_MODEL = os.getenv('GOOGLE_GENAI_MODEL', 'models/text-bison-001')
 
--- app/genai.py
+++ app/genai.py
@@ -1,1 +1,35 @@
 #Here we'll call the GenAI API to generate book summaries based on the book information retrieved from the Google Books API
+# Importing libraries to be used for loading the API
+from google import genai
+from config import GEMINI_API_KEY
+
+# Setting API key
+api_key = GEMINI_API_KEY
+
+# Configure the client
+client = genai.Client(api_key=api_key)
+
+# Creating function to generate summary based on title and author
+def generate_summary(title, author):
+    '''
+    Generates the summary of a book based on title and author using GenAI
+    '''
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=f"Generate a 1-3 sentence summary of the book {title} by {author}"
+    )
+    return response.text
+
+# Quick test to ensure it works as expected:
+def main():
+    '''
+    Main function to test the generate_summary function with sample book data.
+    '''
+    title = "The Hunger Games"
+    author = "Suzanne Collins"
+
+    print(generate_summary(title, author))
+
+
+if __name__ == "__main__":
+    main()
